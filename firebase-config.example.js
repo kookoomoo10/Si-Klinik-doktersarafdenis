5de6c29b41263dd170e925d9cd71ba087a2a9add
kookoomoo10 <kookoomoo10@users.noreply.github.com>
2026-04-09 05:56:13 +0000

    deploy: 58bc737f4c56a005a7a5d1047644584189dfdbdd

---
+++ firebase-config.example.js
@@ -0,0 +1,17 @@
+window.FIREBASE_CONFIG = {
+  apiKey:            "GANTI_DENGAN_apiKey_KAMU",
+  authDomain:        "GANTI_DENGAN_authDomain_KAMU",
+  projectId:         "GANTI_DENGAN_projectId_KAMU",
+  storageBucket:     "GANTI_DENGAN_storageBucket_KAMU",
+  messagingSenderId: "GANTI_DENGAN_messagingSenderId_KAMU",
+  appId:             "GANTI_DENGAN_appId_KAMU"
+};
+
+window.ROLE_MAP = {
+  "UID_DOKTER_DARI_FIREBASE_AUTH" : "dokter",
+  "UID_STAFF_DARI_FIREBASE_AUTH"  : "staff",
+};
+
+window.GCAL_CLIENT_ID = "CLIENT_ID.apps.googleusercontent.com";
+
+window.WA_DOKTER = "628xxxxxxxxxx";
---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+window.FIREBASE_CONFIG = {
+  apiKey:            "AIzaSyCxkRNLIjZcKa9cxBJaZPPZQTISgrWvl10",
+  authDomain:        "klinik-nyeri-saraf.firebaseapp.com",
+  projectId:         "klinik-nyeri-saraf",
+  storageBucket:     "klinik-nyeri-saraf.firebasestorage.app",
+  messagingSenderId: "18806254953",
+  appId:             "1:18806254953:web:c9ea5dfefcd174ff6121f8"
+};
+window.ROLE_MAP = {"Kfv4HZH6sIYWKvRcLBmG8Ni91C32":"dokter","nW7MvqngVGZEIcxLRQAr0RYosUg1":"staff"};
+window.WA_DOKTER = "6282277871593";
+window.GCAL_CLIENT_ID = "";
